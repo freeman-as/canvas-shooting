7d737176e0ca9e6d8f0416a5e093245f73a20a9c
freeman-as <freeman.by2018@gmail.com>
2020-12-27 21:54:56 +0900

    player start animation.

--- script/script.js
+++ script/script.js
@@ -5,8 +5,13 @@
   const BG_COLOR = '#eeeeee';
   let util, canvas, ctx, image;
   let startTime = null;
-  let playerX = CANVAS_HEIGHT / 2;
+  let playerX = CANVAS_WIDTH / 2;
   let playerY = CANVAS_HEIGHT / 2;
+  const PLAYER_START_Y = CANVAS_HEIGHT - 100;
+  // 自機が登場中かどうかのフラグ
+  let isComing = false;
+  // 登場演出を開始した際のタイムスタンプ
+  let comingStart = null;
 
   
   window.addEventListener('load', () => {
@@ -27,17 +32,38 @@
   function initialize() {
     canvas.width = CANVAS_WIDTH;
     canvas.height = CANVAS_HEIGHT;
+
+    isComing = true;
+    comingStart = Date.now();
+    playerY = CANVAS_HEIGHT;
   }
   
   function render() {
+    ctx.globalAlpha = 1.0;
+
     // 描画の度に塗りつぶす
     util.drawRect(0, 0, canvas.width, canvas.height, BG_COLOR);
 
-    // 現在までの経過時間をミリ秒を秒に変換
-    let nowTime = (Date.now() - startTime) / 1000;
+    if (isComing) {
+      let justTime = Date.now();
+      let comingTime = (justTime - comingStart) / 1000;
+      playerY = CANVAS_HEIGHT - comingTime * 50;
 
-    let s = Math.sin(nowTime);
-    let x = s * 100.0;
+      if (playerY <= PLAYER_START_Y) {
+        isComing = false;
+        playerY = PLAYER_START_Y;
+      }
+
+      if (justTime % 100 < 50) {
+        ctx.globalAlpha = 0.5;
+      }
+    }
+
+    // 現在までの経過時間をミリ秒を秒に変換
+    // let nowTime = (Date.now() - startTime) / 1000;
+
+    // let s = Math.sin(nowTime);
+    // let x = s * 100.0;
     ctx.drawImage(image, playerX, playerY);
 
     requestAnimationFrame(render);
@@ -45,6 +71,8 @@
 
   function eventSetting() {
     window.addEventListener('keydown', (event) => {
+      if (isComing) return;
+
       switch (event.key) {
         case 'ArrowLeft':
           playerX -= 10;
